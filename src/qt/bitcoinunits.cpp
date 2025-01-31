38249de4683b3a902678b502105735f3693a0caf
Julian W <julian.waciewski@rwth-aachen.de>
2025-01-31 17:57:07 -0500

    Rename names

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("CSS");
+    case mBTC: return QString("mCSS");
+    case uBTC: return QString::fromUtf8("μCSS");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Csscoins");
+    case mBTC: return QString("Milli-Csscoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Csscoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
