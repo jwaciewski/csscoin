2ce736949f2c2574ff6d6d8c6449a825444d5739
Julian W <julian.waciewski@rwth-aachen.de>
2025-02-01 15:41:22 -0500

    Change halving rate, retarget rate and number of blocks per minute

--- src/main.cpp
+++ src/main.cpp
@@ -1090,13 +1090,13 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 100 * COIN;
 
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 840000); // Csscoin: 840k blocks in ~4 years
+    nSubsidy >>= (nHeight / 100000); // Csscoin: 100k blocks
 
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 3.5 * 24 * 60 * 60; // Csscoin: 3.5 days
-static const int64 nTargetSpacing = 2.5 * 60; // Csscoin: 2.5 minutes
+static const int64 nTargetTimespan = 2 * 24 * 60 * 60; // Csscoin: 3.5 days
+static const int64 nTargetSpacing = 20 * 60; // Csscoin: 2.5 minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 //
