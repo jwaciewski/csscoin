7ca2a10aff520c5c8cd56f81b78ac364717b1d6d
Julian W <julian.waciewski@rwth-aachen.de>
2025-02-01 10:55:30 -0500

    Change mainet connection parameters

--- src/main.cpp
+++ src/main.cpp
@@ -3079,7 +3079,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Csscoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0xf7, 0xcc, 0xb3, 0xd9 }; // Csscoin: increase each by adding 2 to bitcoin's value.
 
 
 void static ProcessGetData(CNode* pfrom)
